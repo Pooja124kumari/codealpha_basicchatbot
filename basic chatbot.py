1440911ba67b20fe79c923a9c8ec1b42b2b6f52e
Pooja124kumari <157134933+Pooja124kumari@users.noreply.github.com>
2025-08-05 10:01:08 -0700

    Add files via upload

---
+++ basic chatbot.py
@@ -0,0 +1,16 @@
+def chatbot():
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Bot: Hi!")
+        elif user_input == "how are you":
+            print("Bot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: I didn't understand that.")
+
+# Start the chatbot
+chatbot()
